019fd7f067f9eb9ef35d1350eaee84167c4d68b4
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2024-11-19 08:53:19 -0400

    Conexion a la base de datos

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-import express from 'express'
+import express, { query } from 'express'
 import cors from 'cors'
 import { createServer } from 'http'
 import mariadb from 'mariadb'
+import { Bearer } from 'permit'
 
 const port = 3000
+
+const permit = new Bearer({
+	query: 'access_token'
+})
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-const db = mariadb.createPool({
-	host: 'localhost',
-	user: '',
-	password: '',
-	database: 'Odontologia',
-	port: 3307
-})
+async function createPool(){
+	const db = mariadb.createPool({
+			host: 'localhost',
+			user: 'root',
+			password: '2402',
+			database: 'pruebas',
+			port: 3306,
+			acquireTimeout: 10000,
+			conexionLimit: 5
+		})
 
-app.post('/api/login', (req, res) => {
-	
-})
+	let connection = await db.getConnection()
 
+	let res = await connection.query('SELECT * FROM users')
+	console.log(res)
 
-const server = createServer(app)
-server.listen(port, () => {
-	console.log('Su puerto es: 3000')
-})
+	connection.end()
+
+	const server = createServer(app)
+	server.listen(port, () => {
+		console.log('Su puerto es: 3000')
+	})	
+}
+
+createPool()
