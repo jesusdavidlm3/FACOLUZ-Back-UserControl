113c53b80274ea86ee6a9dbf900345f8758b61d8
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2024-11-19 16:04:12 -0400

    Avances implementacion de JWB.

--- index.js
+++ index.js
@@ -2,41 +2,58 @@ import express, { query } from 'express'
 import cors from 'cors'
 import { createServer } from 'http'
 import mariadb from 'mariadb'
-import { Bearer } from 'permit'
+import jwt from 'jsonwebtoken'
 
 const port = 3000
-
-const permit = new Bearer({
-	query: 'access_token'
-})
+const secretKey = 'secret'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-async function createPool(){
-	const db = mariadb.createPool({
-			host: 'localhost',
-			user: 'root',
-			password: '2402',
-			database: 'pruebas',
-			port: 3306,
-			acquireTimeout: 10000,
-			conexionLimit: 5
-		})
+const db = mariadb.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '2402',
+	database: 'pruebas',
+	port: 3306,
+	acquireTimeout: 10000,
+	conexionLimit: 5
+})
 
-	let connection = await db.getConnection()
+// function verifyToken(req, res, header){
+// 	const header = req.header
+// 	const token = header.split(" ")[0]
+// 	if(!token){
+// 		return res.status(401).json({message: "Token not provided"})
+// 	}
+// 	try{
+// 		const payload = jwt.verify(token, secretKey)
+// 		req.username =    
+// 	}
+// }
 
-	let res = await connection.query('SELECT * FROM users')
-	console.log(res)
+app.post('/api/login', (req, res) => {
+	try{
+		console.log('ejectutando')
+		const { id, password } = req.body
+		let connection = db.getConnection()
+		// let query = connection.query('SELECT * FROM users')
+		// console.log(query)
+	}catch(err){
+		console.log(err)
+		res.status(500).send('error del servidor')
+	}finally{
+		connection.end()
+	}
+})
 
-	connection.end()
+// app.post('/api/registerUser', verifyToken, (req, res) => {
 
-	const server = createServer(app)
-	server.listen(port, () => {
-		console.log('Su puerto es: 3000')
-	})	
-}
+// })
 
-createPool()
+const server = createServer(app)
+server.listen(port, () => {
+	console.log('Su puerto es: 3000')
+})	
